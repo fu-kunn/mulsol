066d859caefa7186628b5f902e4e6f56a877c523
fu-kunn <sky53908@gmail.com>
2022-03-10 05:14:15 +0000

    [Fix]企業の詳細ページ、業種の表示

---
+++ db/migrate/20220310011940_add_sector_id_to_customers.rb
@@ -0,0 +1,5 @@
+class AddSectorIdToCustomers < ActiveRecord::Migration[6.1]
+  def change
+    add_column :customers, :sector_id, :integer
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_09_123454) do
+ActiveRecord::Schema.define(version: 2022_03_10_011940) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -89,6 +89,7 @@ ActiveRecord::Schema.define(version: 2022_03_09_123454) do
     t.string "password"
     t.boolean "withdraw", default: false, null: false
     t.integer "issue_id"
+    t.integer "sector_id"
     t.index ["email"], name: "index_customers_on_email", unique: true
     t.index ["reset_password_token"], name: "index_customers_on_reset_password_token", unique: true
   end
