cabeec1fe89571f2387fc81730a4b2bac9265ffb
fu-kunn <sky53908@gmail.com>
2022-03-24 07:07:26 +0000

    [Fix]contactのリレーション変更

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,4 +1,8 @@
 class Contact < ApplicationRecord
-  belongs_to :customer
-  has_many :notifications, dependent: :destroy
+  # belongs_to :customer
+  # has_many :notifications, dependent: :destroy
+  validates :company_name, presence: true
+  validates :email, presence: true
+  validates :subject, presence: true, length: { maximum: 40 }
+  validates :message, presence: true, length: { maximum: 500 }
 end
