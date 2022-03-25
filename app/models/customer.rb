1d2e4e16a3d50202f19143436abed7b892640fed
fu-kunn <sky53908@gmail.com>
2022-03-25 07:08:03 +0000

    [Fix]会員の新規登録/エラー表示

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,18 +1,18 @@
 class Customer < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
 
   validates :company_name, presence: true
   validates :company_name_kana, presence: true
+  validates :post_code, presence: true
+  validates :address, presence: true
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :first_name_kana, presence: true
   validates :last_name_kana, presence: true
-  validates :post_code, presence: true
-  validates :address, presence: true
   validates :tel, presence: true
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
 
   has_many :issues, dependent: :destroy
   belongs_to :sector, optional: true
